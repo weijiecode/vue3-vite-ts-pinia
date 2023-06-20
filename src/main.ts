bd62cb57da2276a08f8d50f9ad68319c55d950c9
weijiecode <245619254@qq.com>
2023-06-20 14:52:05 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '@/styles/index.scss'
+// 忽略引入国际化报的ts错误
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import App from '@/App.vue'
+
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn,
+  });
+// svg配置
+import 'virtual:svg-icons-register'
+import SvgIcon from '@/components/SvgIcon/index.vue'
+app.component('SvgIcon', SvgIcon)
+app.mount('#app')
